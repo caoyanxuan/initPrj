25de35d0633147fbf291f8b805f543c89fa8e528
caoyx <742356568@qq.com>
2018-01-05 15:08:20 +0800

    init blog

---
+++ app.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var routes = require('./routes/index');
+var settings = require('./settings');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var flash = require('connect-flash');
+var app = express();
+
+// view engine setup
+app.set('port', process.env.PORT || 30000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(flash());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: settings.cookieSecret,
+  key: settings.db,//cookie name
+  cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//30 days
+  store: new MongoStore({
+    db: settings.db,
+    host: settings.host,
+    port: settings.port,
+    url: settings.url,
+  })
+}));
+
+routes(app);
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+module.exports = app;
---
+++ settings.js
@@ -0,0 +1,7 @@
+module.exports = { 
+    cookieSecret: 'myblog', 
+    db: 'blog', 
+    host: 'localhost',
+    port: 27017,
+    url: "mongodb://localhost:27017/blog"
+  };
